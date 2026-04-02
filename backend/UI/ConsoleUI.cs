f20eb19cc1867812dcab6a379e6a50ef2673c384
Hataz511 <zekahate758@gmail.com>
2026-04-02 09:44:57 +0200

    Update ConsoleUI.cs

--- backend/UI/ConsoleUI.cs
+++ backend/UI/ConsoleUI.cs
@@ -1,40 +1,26 @@
-public class Menu
+public class ConsoleUI
 {
-    private ChemicalService _service;
+    private readonly ChemicalService _service;
 
-    public Menu(ChemicalService service)
+    public ConsoleUI(ChemicalService service)
     {
         _service = service;
     }
 
     public void Start()
     {
-        while (true)
+        Console.WriteLine("1. Add Chemical");
+        var choice = Console.ReadLine();
+
+        if (choice == "1")
         {
-            Console.WriteLine("1. Add Chemical");
-            Console.WriteLine("2. Show All");
-
-            var choice = Console.ReadLine();
-
-            if (choice == "1")
+            var chem = new Chemical
             {
-                var chem = new Chemical
-                {
-                    Id = new Random().Next(1, 1000),
-                    Name = "Test",
-                    Quantity = 10
-                };
+                Name = "Test",
+                Quantity = 10
+            };
 
-                _service.AddChemical(chem);
-            }
-            else if (choice == "2")
-            {
-                var list = _service.GetAll();
-                foreach (var c in list)
-                {
-                    Console.WriteLine(c.Name);
-                }
-            }
+            _service.AddChemical(chem);
         }
     }
 }
