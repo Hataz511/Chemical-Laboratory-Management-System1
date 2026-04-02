10e1e04ecac96a9b75e596bad7d128f6e3eeb67a
Hataz511 <zekahate758@gmail.com>
2026-04-02 09:37:20 +0200

    Update  FileRepository.cs

--- backend/Data/ FileRepository.cs
+++ backend/Data/ FileRepository.cs
@@ -1,39 +1,34 @@
-using System.Text;
+using System.IO;
 
 public class FileRepository<T> : IRepository<T> where T : class
 {
-    private readonly string _filePath;
-    private List<T> _items;
+    private List<T> _data;
+    private string _filePath;
 
     public FileRepository(string filePath)
     {
         _filePath = filePath;
-        _items = new List<T>();
-
-        if (File.Exists(_filePath))
-        {
-            var lines = File.ReadAllLines(_filePath);
-            foreach (var line in lines)
-            {
-                // Deserialize manually or with JSON
-                _items.Add(System.Text.Json.JsonSerializer.Deserialize<T>(line)!);
-            }
-        }
+        _data = new List<T>();
     }
 
-    public List<T> GetAll() => _items;
+    public List<T> GetAll()
+    {
+        return _data;
+    }
 
-    public T? GetById(Guid id)
-        => _items.FirstOrDefault();
+    public T GetById(int id)
+    {
+        return _data.FirstOrDefault();
+    }
 
     public void Add(T entity)
-        => _items.Add(entity);
+    {
+        _data.Add(entity);
+    }
 
     public void Save()
     {
-        var lines = _items.Select(i =>
-            System.Text.Json.JsonSerializer.Serialize(i));
-
-        File.WriteAllLines(_filePath, lines);
+        File.WriteAllText(_filePath, "Saved (simulim)");
     }
+}
 }
